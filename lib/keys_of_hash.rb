3204ec26ebd3916da58a235a21739aa38895be05
jchamberlain909 <jchamberlain909@gmail.com>
2018-05-15 13:40:51 -0400

    implemented keys_of method

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    return_arr = []
+    self.each do |key, value|
+      arguments.each do |e|
+        if(e==value)
+          return_arr << key
+        end
+      end
+    end
+    return_arr
+
   end
 end
