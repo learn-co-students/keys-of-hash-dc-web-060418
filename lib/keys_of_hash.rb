f2ae7d6d80266727c1406610e1baa4e8e2d4e663
github username <github email address>
2018-05-24 19:00:27 -0400

    Documentation for the win

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require "pry"
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    # This method should take an an undefined number of arguments, using the splat operator,
+    #and return an array with every key from the hash whose value matches the value(s) given as an argument.
+    keys_at_value_array =[]
+    self.each do |key, value|
+      if arguments.include?(value) == true
+        keys_at_value_array.push(key)
+      end
+    end
+    keys_at_value_array
   end
+
 end
